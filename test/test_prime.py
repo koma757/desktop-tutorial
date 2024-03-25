cb1160c22766c0303966ea90efcb3b067564554d
koma <t.komaba.22.11.724@4nonome.com>
2024-03-25 14:31:52 +0900

    Squashed commit of the following:
    commit 05cf0aa5cdd65e19a51b148c207964f27d4c1de8
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 19 20:20:02 2024 +0900
    
        Delete README.md
    
    commit a9ffad2f05a5ad5d02280bd13f60d29019ef480f
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 19 20:19:50 2024 +0900
    
        Delete README.txt
    
    commit ab69aa8e31f7a4a77190c45706d7a17503c2f2aa
    Merge: 16b9d21 0b9b46c
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 19 20:15:33 2024 +0900
    
        Merge pull request #4 from koma757/koma757-patch-1
    
        Create blank.yml
    
    commit 0b9b46c7b209e8f55feb5bfef184a46eef766ea9
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 19 20:11:28 2024 +0900
    
        Update blank.yml
    
    commit 16b9d2157e41ab6047619aedcf72838b365174ba
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 19 19:26:11 2024 +0900
    
        test
    
    commit a7394634c15833e5cced37da4940f7b4b99a44ab
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 19 19:13:02 2024 +0900
    
        Update blank.yml
    
    commit 38f84386b6639bd35be5dd61467c68da041a656d
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 19 19:07:47 2024 +0900
    
        unittestをpytestに変更
    
    commit 70a3d4d7d979506dc6f200df01922351ba6c5169
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 22:09:39 2024 +0900
    
        Update blank.yml
    
    commit 00ee6e71c0f43934f2ce9af4ab92f5455d20bae1
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 22:02:29 2024 +0900
    
        Update blank.yml
    
    commit df2ebc05d53e8a05d12f6c03dc9852550bd34c4f
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 17:49:36 2024 +0900
    
        Update blank.yml
    
    commit e238d77f4a49232b78e784e7c78b3f1486025918
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 17:31:02 2024 +0900
    
        Update blank.yml
    
    commit e8d61cf50e568a4a92d1df6b57d10b3d9e5cecb3
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 17:23:22 2024 +0900
    
        Update blank.yml
    
    commit d0ee270f014eee0f12d7a96f9973f386b5df23e9
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 17:05:32 2024 +0900
    
        Update blank.yml
    
    commit ca415e840e3b7cfd7b5a85a8e6e3993fc19231a9
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 16:45:33 2024 +0900
    
        Update blank.yml
    
    commit e2ff689ab94a454126887618e19d35acf4dad65e
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 16:35:59 2024 +0900
    
        Update blank.yml
    
    commit 28487843d3e09e3a2d77e89d27706a2a09d8c6e9
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Wed Mar 6 16:04:19 2024 +0900
    
        Update blank.yml
    
    commit 5c2ff067205418f53c63112f12805cae62abd4b6
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 5 18:05:06 2024 +0900
    
        Update blank.yml
    
    commit 63f7ac48fd3f5d07a4c3b961457a02cf510b3a95
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Tue Mar 5 17:47:46 2024 +0900
    
        Update blank.yml
    
    commit f4071178b969277d9f148158a97a5d876604c07e
    Author: koma <t.komaba.22.11.724@4nonome.com>
    Date:   Sun Mar 3 13:07:47 2024 +0900
    
        Create blank.yml

--- test/test_prime.py
+++ test/test_prime.py
@@ -1,38 +1,39 @@
-import unittest
-
 from prime import is_prime
 
 
-class TestPrime(unittest.TestCase):
-
-    def test_is_prime(self):
-        self.assertFalse(is_prime(1))
-        self.assertTrue(is_prime(2))
-        self.assertTrue(is_prime(3))
-        self.assertFalse(is_prime(4))
-        self.assertTrue(is_prime(5))
-        self.assertFalse(is_prime(6))
-        self.assertTrue(is_prime(7))
-        self.assertFalse(is_prime(8))
-        self.assertFalse(is_prime(9))
-        self.assertFalse(is_prime(10))
-        self.assertTrue(is_prime(11))
-        self.assertTrue(is_prime(13))
-        self.assertTrue(is_prime(17))
-        self.assertTrue(is_prime(19))
-        self.assertTrue(is_prime(23))
-        self.assertFalse(is_prime(25))
-        self.assertTrue(is_prime(29))
-        self.assertFalse(is_prime(30))
-        self.assertTrue(is_prime(31))
-        self.assertFalse(is_prime(33))
-        self.assertTrue(is_prime(37))
-        self.assertTrue(is_prime(41))
-        self.assertTrue(is_prime(43))
-        self.assertFalse(is_prime(49))
-        self.assertTrue(is_prime(53))
-        # 他のテストケースも同様に追加
+def test_is_prime():
+    assert not is_prime(1)
+    assert is_prime(2)
+    assert is_prime(3)
+    assert not is_prime(4)
+    assert is_prime(5)
+    assert not is_prime(6)
+    assert is_prime(7)
+    assert not is_prime(8)
+    assert not is_prime(9)
+    assert not is_prime(10)
+    assert is_prime(11)
+    assert is_prime(13)
+    assert is_prime(17)
+    assert is_prime(19)
+    assert is_prime(23)
+    assert not is_prime(25)
+    assert is_prime(29)
+    assert not is_prime(30)
+    assert is_prime(31)
+    assert not is_prime(33)
+    assert is_prime(37)
+    assert is_prime(41)
+    assert is_prime(43)
+    assert not is_prime(49)
+    assert is_prime(53)
+    # 他のテストケースも同様に追加
 
 
+# コードの最後に、以下を追加します。
 if __name__ == "__main__":
-    unittest.main()
+    # このブロックが実行されるとき、pytestコマンドによって
+    # ファイルが直接実行されたことを意味します。
+    # この場合、pytest.main()を呼び出してテストを実行します。
+    import pytest
+    pytest.main()
